1bcbb9aed932cf823b3aa19b08476685decb7206
Maximilian Shaw <maximilianyoung52@gmail.com>
2022-06-15 12:46:39 -0400

    edited bug in user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,15 +1,57 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.post('/', async(req, res) => {
-    try {
-
-    } catch(err) {
-        res.status(500).json(err);
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.username = newUser.username;
+      req.session.loggedIn = true;
+      res.json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+    if (!user) {
+      res.status(400).json({ message: "User not found!" });
+      return;
+    }
+    const validatePassword = user.checkPassword(req.body.password);
+    if (!validatePassword) {
+      res.status(400).json({ message: "User not found!" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.username = newUser.username;
+      req.session.loggedIn = true;
+      res.json({ user, message: "You are logged in!" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            req.status(200).end();
+        })
+    } else {
+        req.status(404).end();
     }
 });
-router.post('/login', async(req, res) => {
-    try {
 
     } catch(err) {
         res.status(500).json(err);
---
+++ controllers/home-routes.js
@@ -0,0 +1,95 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const { User, Post, Comment } = require('../models');
+
+
+router.get('/user/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id, {
+      include: [
+        {
+          model: user,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('user', {
+      ...user,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: user }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req,res) => {
+  try{
+    const postData = await Post.findByPk(req.params.id, {
+      include: [User, {
+        model: comment,
+        include: [User]
+      }]
+    })
+    if(postData) {
+      const post = postData.get({ plain: true })
+      res.render('single-post', {post})
+    } else {
+      res.status(404).json({message: 'Post Not Found!'})
+    }
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+})
+
+
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/logout', (req, res) => {
+  // If the user logs out, redirect the request sign in
+  if (req.session.logged_out) {
+    res.redirect('/login');
+    return;
+  }
+});
+
+router.get('/signup', async (res,req) => {
+  if(req.session.logged_in) {
+    res.redirect('/')
+    return;
+  }
+  res.render('signup')
+})
+
+module.exports = router;
