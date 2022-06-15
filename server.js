902d253c5dcae584f238af789038c6e5e1b61775
Maximilian Shaw <maximilianyoung52@gmail.com>
2022-06-15 11:27:51 -0400

    edited files within public/js and debugged server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const sequelize = require('./config/connections');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers')
+const helpers = require('./utils/helper')
 
 
 const app = express();
