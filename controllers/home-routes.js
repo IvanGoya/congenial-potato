43bd004074f5f88fa50d03b0d17f8760f3f0999d
Ivan Goya <ivan.goya98@gmail.com>
2022-06-15 11:35:30 -0400

    implementing the home-routes file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,47 +1,22 @@
 const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const { User, Post, Comment } = require('../models');
 
-router.get('/', async (req, res) => {
+
+router.get('/user/:id', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const userData = await User.findByPk(req.params.id, {
       include: [
         {
-          model: User,
+          model: user,
           attributes: ['name'],
         },
       ],
     });
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const user = userData.get({ plain: true });
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
+    res.render('user', {
+      ...user,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -55,7 +30,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      include: [{ model: User }],
     });
 
     const user = userData.get({ plain: true });
@@ -69,14 +44,52 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+router.get('/post/:id', async (req,res) => {
+  try{
+    const postData = await Post.findByPk(req.params.id, {
+      include: [User, {
+        model: Comment,
+        include: [User]
+      }]
+    })
+    if(postData) {
+      const post = postData.get({ plain: true })
+      res.render('single-post', {post})
+    } else {
+      res.status(404).json({message: 'Post Not Found!'})
+    }
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+})
+
+
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
   }
-
   res.render('login');
 });
+
+router.get('/logout', (req, res) => {
+  // If the user logges out, redirect the request sign in
+  if (req.session.logged_out) {
+    res.redirect('/login');
+    return;
+  }
+  res.render('logged out');
+});
+
+router.get('/signup', async (res,req) => {
+  if(req.session.logged_in) {
+    res.redirect('/')
+    return;
+  }
+  res.render('signup')
+})
 
 module.exports = router;
