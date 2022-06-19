8837aa300b53b547a4fe0ca4cf782859f991b5e5
bnagel <nagelb500@gmail.com>
2022-06-19 19:40:55 -0400

    post route initial work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const kanbanRoutes = require('./kanban-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/user', userRoutes);
 router.use('/kanban', kanbanRoutes);
+router.use('/post', postRoutes)
 
 module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,25 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
-  
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
+    const post = document.querySelector('.new-post-item').value.trim();
+
+    if (post) {
+      const response = await fetch(`/api/post`, {
         method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
+        body: JSON.stringify({ postTitle }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/');
       } else {
         alert('Failed to create project');
       }
     }
   };
   
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/projects/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete project');
-      }
-    }
-  };
-  
-  document
-    .querySelector('.new-project-form')
-    .addEventListener('submit', newFormHandler);
-  
-  document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler);
+  document.querySelector('.new-post-button').addEventListener('click', () => {
+    newFormHandler();
+  });
