ed46155b62c39605020d61b58c6b1a3ad85d69c2
Maximilian Shaw <maximilianyoung52@gmail.com>
2022-06-20 19:14:55 -0400

    changes to comments

--- public/js/global.js
+++ public/js/global.js
@@ -5,8 +5,9 @@ const openModal = document.querySelector('.open-button');
 const closeModal = document.querySelector('.close-button');
 const submitBtn = document.querySelector('#submitBtn');
 const deleteBtn = document.querySelector('#delete-button');
-const commentBtn = document.querySelector('#comment-button');
-
+const commentBtn = document.querySelector('#comment-btn');
+const commentBox = document.querySelector('#comment-box')
+console.log("jnaskdjn");
 openModal.addEventListener('click', () => {
     modal.showModal();
 })
@@ -23,8 +24,11 @@ deleteBtn.addEventListener('click', () => {
     deletePostHandler();
 })
 
-// Format Date logic will go below
-
 commentBtn.addEventListener('click', () => {
-
+    console.log("alsjkndlak");
+    commentBox.style.display = "block"
 })
+
+
+
+// Format Date logic will go below
