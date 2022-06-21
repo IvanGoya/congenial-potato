e4170902fd6da9760337470d51019f7ebbe39f58
Maximilian Shaw <maximilianyoung52@gmail.com>
2022-06-20 22:14:06 -0400

    completed comment functionality

--- public/js/createComments.js
+++ public/js/createComments.js
@@ -4,8 +4,9 @@ const newComment = async(event) => {
     event.preventDefault();
     const comment_body = document.querySelector('#comment-contents').value.trim()
     const postId = window.location.toString().split('/')[window.location.toString().split('/').length - 1 ];
+    console.log(JSON.stringify({comment_body, postId}))
     if (comment_body) {
-        const res = await fetch('/api/comment-routes/', {
+        const res = await fetch('/api/comment/', {
             method: 'POST',
             body: JSON.stringify({comment_body, postId}),
             headers: {'Content-Type': 'application/json'}
