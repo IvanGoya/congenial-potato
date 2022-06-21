8d07d8199b361cde9bf7fb8037607e7ee4f92c72
Drew Kempen <drewkempen98@gmail.com>
2022-06-20 22:01:42 -0400

    fix comment route IVAN

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const kanbanRoutes = require('./kanban-routes');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes')
 
 router.use('/user', userRoutes);
 router.use('/kanban', kanbanRoutes);
 router.use('/post', postRoutes)
+router.use('/comment', commentRoutes)
 
 module.exports = router;
